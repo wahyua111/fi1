5b820402ec3bd07495d4c781e1dc63617ae00f0b
Mondar111 <86989223+Mondar111@users.noreply.github.com>
2022-01-28 08:38:00 +0700

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,6 +1,6 @@
 # (©)Codexbotz
-# Recode by @mrismanaziz
-# t.me/SharingUserbot & t.me/Lunatic0de
+# Recode by @cieemarah
+# t.me/ohbabysini & t.me/familynvn
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
